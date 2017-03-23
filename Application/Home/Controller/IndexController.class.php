27c744915e285c2917705aa5b9d9fcc392634ff9
zhangtao <657920579@qq.com>
2017-03-23 13:08:51 +0800

    added

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,7 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        echo "懵逼树上懵逼果，懵逼树下你和我，懵逼树前做交易，懵逼树后泄浴火".'--鲁迅';
+        echo "懵逼树上懵逼果，懵逼树下你和我，懵逼树前做交易，懵逼树后泄浴火".'--鲁迅'."<br>";
         echo"Authored By 张涛".date('Y-m-d',time());
     }
 }
