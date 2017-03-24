1f15fda12768391b3b63bb5f2ddaa6db5806c4a4
zhangtao <657920579@qq.com>
2017-03-24 16:23:02 +0800

    added

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,17 +1,9 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-class IndexController extends CommonController {
+class IndexController extends Controller {
 
-    /**
-     * 初始化
-     */
-    public function _initialize(){
-        parent::_initialize();
-        if(!$this->isLogin){
-            $this->redirect('home/login/index');
-        }
-    }
+
     /**
      * 首页界面
      */
