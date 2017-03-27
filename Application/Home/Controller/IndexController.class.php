c8f484af559df73f02dffc8eb4861b4a4271ac08
zhangtao <657920579@qq.com>
2017-03-27 18:22:21 +0800

    added

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -42,7 +42,15 @@ class IndexController extends CommonController{
      */
     public function detail($id){
         $id = intval($id);
-
+        $commentModel = M('Comment');
+        //>> 默认查询导演
+        $rows = $commentModel->where(['type'=>1,'movie_id'=>$id])->select();
+        $directorArr = [];
+        $count = 0;
+        if(!empty($rows)){
+            $count = ceil(count($rows) / 12);
+            $directorArr = $this->pagination($rows,1,12);
+        }
         // 查看项目是否存在
         $info = M('project as a')
             ->join('left join an_project_survey as b on b.project_id = a.id')
@@ -52,8 +60,57 @@ class IndexController extends CommonController{
             $this->error('项目不存在');
             exit;
         }
-        $this->assign('info',$info);
-        $this->display('detail');
+        $this->assign([
+            'info'=>$info,
+            'comment'=>$directorArr,
+            'count'=>$count,
+        ]);
+        $this->display('index/detail');
     }
 
+    /**
+     * 分页查询
+     */
+    public function pageSelect(){
+
+        $paramArr = $_REQUEST;
+
+        if(!empty($paramArr)){
+            if(isset($paramArr['pgNum']) && !empty($paramArr['pgNum']) && is_numeric($paramArr['pgNum'])){
+                $pgNum = $paramArr['pgNum'];
+            }else{
+                $pgNum = 1;
+            }
+            if(isset($paramArr['pgSize']) &&!empty($paramArr['pgSize']) && is_numeric($paramArr['pgSize'])){
+                $pgSize = $paramArr['pgSize'];
+            }else{
+                $pgSize = 12;
+            }
+            $rows = M('Comment')->where(['movie_id'=>$paramArr['type']])->select();
+
+            if(!empty($rows)){
+                $directorArr = $this->pagination($rows,$pgNum,$pgSize);
+                $this->ajaxReturn([
+                    'directorArr'=>$directorArr,
+                    'status'=>1
+                ]);
+            }else{
+                die($this->_printError(''));
+            }
+        }else{
+            die($this->_printError(''));
+        }
+    }
+
+    /**
+     * 分页工具
+     */
+    public function pagination($data = [],$pgNum,$pgSize){
+
+        if(empty($data)) return false;
+
+        $start = ($pgNum - 1) * $pgSize;
+        $sliceArr = array_slice($data,$start,$pgSize);
+        return $sliceArr;
+    }
 }
