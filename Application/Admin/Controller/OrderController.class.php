55e08fd258259a21b95852e60ebee06438e1daeb
guokang <guokang@ecshy.cn>
2017-03-31 16:41:01 +0800

    df

--- Application/Admin/Controller/OrderController.class.php
+++ Application/Admin/Controller/OrderController.class.php
@@ -171,28 +171,29 @@ class OrderController extends CommonController
     /**
      * 导出支持订单列表
      */
-    public function exportDataSupport(){
+    public function exportDataSupport()
+    {
 
         $where = [];
-        $order_number = I('get.order_number','','strip_tags');
-        $username = I('get.username','','strip_tags');
-        $name = I('get.project_name','','strip_tags');
+        $order_number = I('get.order_number', '', 'strip_tags');
+        $username = I('get.username', '', 'strip_tags');
+        $name = I('get.project_name', '', 'strip_tags');
         $start_time = strtotime(I('get.start_time'));
         $end_time = strtotime(I('get.end_time'));
-        if($order_number){
-            $where['a.order_number'] = ['like',"%$order_number%"];
+        if ($order_number) {
+            $where['a.order_number'] = ['like', "%$order_number%"];
         }
-        if($username){
-            $where['c.username'] = ['like',"%$username%"];
+        if ($username) {
+            $where['c.username'] = ['like', "%$username%"];
         }
-        if($name){
-            $where['b.name'] = ['like',"%$name%"];
+        if ($name) {
+            $where['b.name'] = ['like', "%$name%"];
         }
-        if($start_time){
-            $where['a.create_time'] = ['egt',$start_time];
+        if ($start_time) {
+            $where['a.create_time'] = ['egt', $start_time];
         }
-        if($end_time){
-            $where['a.create_time'] = ['elt',$end_time];
+        if ($end_time) {
+            $where['a.create_time'] = ['elt', $end_time];
         }
 
         //查询出所有下载订单
@@ -202,22 +203,22 @@ class OrderController extends CommonController
             ->join('left join an_member as c on c.id=a.member_id')
             ->where($where)
             ->select();
-        foreach ($rows as &$info){
-            $info['create_time'] = date('Y-m-d',$info['create_time']);
+        foreach ($rows as &$info) {
+            $info['create_time'] = date('Y-m-d', $info['create_time']);
         }
         unset($info);
 
-        $xlsCell  = array(
-            array('id','编号'),
-            array('order_number','订单号'),
-            array('username','支持用户'),
-            array('project_name','支持项目'),
-            array('support_money','支持金额'),
-            array('create_time','支持时间'),
+        $xlsCell = array(
+            array('id', '编号'),
+            array('order_number', '订单号'),
+            array('username', '支持用户'),
+            array('project_name', '支持项目'),
+            array('support_money', '支持金额'),
+            array('create_time', '支持时间'),
         );
 
-        $this->exportExcel(date('Y-m-d').'_支持订单',$xlsCell,$rows);
-
+        $this->exportExcel(date('Y-m-d') . '_支持订单', $xlsCell, $rows);
+    }
 
 
     /**
