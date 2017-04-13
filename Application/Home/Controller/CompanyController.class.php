c48a8aa3243659125a6bc0748fd4249d62e36dad
guokang <guokang@ecshy.cn>
2017-04-13 16:54:05 +0800

    12

--- Application/Home/Controller/CompanyController.class.php
+++ Application/Home/Controller/CompanyController.class.php
@@ -4,13 +4,13 @@ namespace Home\Controller;
 class CompanyController extends CommonController
 {
     public function index(){
-        $this->display('company/index');
-    }
-    public function about(){
         // 查询出合作机构
         $cooper = M('Cooper')->select();
         // 分配合作机构数据
-        $this->assign('cooper',$cooper);
+        $this->assign('coopers',$cooper);
+        $this->display('company/index');
+    }
+    public function about(){
         $this->display('company/about');
     }
 }
