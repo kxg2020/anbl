15f724b53df37efc394f62132811cdfa847e9c27
zhangtao <657920579@qq.com>
2017-04-18 14:53:28 +0800

    dd

--- Application/Home/Controller/PersonalController.class.php
+++ Application/Home/Controller/PersonalController.class.php
@@ -124,7 +124,7 @@ class PersonalController extends CommonController{
 
 
         //>> 查询当前用户的支持情况
-        $rows = M('MemberSupport as a')->field('a.id as aid,b.*')
+        $rows = M('MemberSupport as a')->field('a.id as aid,a.support_money,b.*')
             ->join('left join an_project as b on a.project_id = b.id')
             ->where(['a.member_id'=>$this->userInfo['id']])
             ->select();
