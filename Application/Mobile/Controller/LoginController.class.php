9e5f594aa71ff41ee95d0c74c5dc6217562c7978
zhangtao <657920579@qq.com>
2017-04-19 10:25:41 +0800

    dd

--- Application/Mobile/Controller/LoginController.class.php
+++ Application/Mobile/Controller/LoginController.class.php
@@ -3,5 +3,11 @@ namespace Mobile\Controller;
 
 class LoginController extends CommonController{
 
-    
+    /*
+     * 展示登录
+     */
+    public function index(){
+
+        $this->display('login/index');
+    }
 }
