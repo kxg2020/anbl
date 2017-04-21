66ed59d640b8d9890773d926f9afc3f6aac4ca2b
guokang <guokang@ecshy.cn>
2017-04-21 11:57:34 +0800

    12

--- Application/Mobile/Controller/CommonController.class.php
+++ Application/Mobile/Controller/CommonController.class.php
@@ -54,6 +54,10 @@ class CommonController extends Controller{
      * 初始化
      */
     public function _initialize(){
+        if(!$this->isMobile()){
+            echo "请使用手机端打开";
+            exit;
+        }
         // 获取系统设置信息
         $this->getSystemInfo();
 
@@ -222,5 +226,23 @@ class CommonController extends Controller{
 
         return $sliceArr;
 
+    }
+    public function isMobile() {
+        $mobile = array();
+        static $mobilebrowser_list ='Mobile|iPhone|Android|WAP|NetFront|JAVA|OperasMini|UCWEB|WindowssCE|Symbian|Series|webOS|SonyEricsson|Sony|BlackBerry|Cellphone|dopod|Nokia|samsung|PalmSource|Xphone|Xda|Smartphone|PIEPlus|MEIZU|MIDP|CLDC';
+        //note 获取手机浏览器
+        if(preg_match("/$mobilebrowser_list/i", $_SERVER['HTTP_USER_AGENT'], $mobile)) {
+            return true;
+        }else{
+            if(preg_match('/(mozilla|chrome|safari|opera|m3gate|winwap|openwave)/i', $_SERVER['HTTP_USER_AGENT'])) {
+                return false;
+            }else{
+                if($_GET['mobile'] === 'yes') {
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        }
     }
 }
