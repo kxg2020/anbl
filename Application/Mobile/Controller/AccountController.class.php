557d5ba6e6d37457c2f045a52bf95886678a8832
zhangtao <657920579@qq.com>
2017-05-05 12:30:58 +0800

    add

--- Application/Mobile/Controller/AccountController.class.php
+++ Application/Mobile/Controller/AccountController.class.php
@@ -370,6 +370,15 @@ class AccountController extends CommonController{
             die($this->_printError('1046'));
         }
 
+
+        //>> 判断用户是否已经有还未审核的充值订单，如果有，就不能再继续充值了
+        $rechargeOrder = M('MemberRecharge')->where(['member_id'=>$row['id'],'is_pass'=>0])->find();
+
+        if(!empty($rechargeOrder)){
+            die($this->_printError('1068'));
+        }
+
+
         if(!empty($paramArr)){
 
             if(isset($paramArr['money']) && !empty($paramArr['money']) && is_numeric($paramArr['money'])){
@@ -496,55 +505,6 @@ class AccountController extends CommonController{
         ]);
     }
 
-    public function test(){
-
-        //>> 判断是否登录
-        if($this->isLogin == 0){
-            $this->redirect('Login/index');
-            return false;
-        }
-
-        //>> 查询支付方式
-        $payMode = M('Pay')->select();
-
-        $this->assign('pay',$payMode);
-        $this->display('account/test');
-    }
-
-    public function upload1(){
-        $str = $_POST['str'];
-        $type = $_POST['type'];
-
-
-        switch($type){
-            case 'image/png':
-                $ext='.png';
-                break;
-            case 'image/jpeg';
-                $ext='.jpeg';
-                break;
-            case 'image/jpeg':
-                $ext='.jpg';
-                break;
-            case 'image/bmp':
-                $ext='.bmp';
-                break;
-            default:
-                $ext='.jpg';
-        }
-        $file_path='./Uploads/'.date('Ymd').'/'.time().$ext;
-        if(!file_exists(dirname($file_path))){
-            mkdir(dirname($file_path),0777,true);
-        }
-        $img_content = str_replace('data:'.$type.';base64,','',$str);
-        $img_content = base64_decode($img_content);
-        $result =file_put_contents($file_path,$img_content);
-        $this->ajaxReturn([
-            'status' => 1,
-            'url' => "http://mobile.araberrimovie.com".$file_path
-        ]);
-
-    }
 
     public function upToken(){
         require '../ThinkPHP/Library/Vendor/Qiniu/autoload.php';
